e21126e8945b2e018492903f31118525bea9674f
Machi0205 <a1030520304@gmail.com>
2019-05-16 21:48:59 +0800

    Initial commit

---
+++ calculator01/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace calculator01
+{
+    partial class MachiCaculator
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.num0 = new System.Windows.Forms.Button();
+            this.dot = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.mult = new System.Windows.Forms.Button();
+            this.negative = new System.Windows.Forms.Button();
+            this.leftbrackets = new System.Windows.Forms.Button();
+            this.rightbrackets = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(12, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(357, 64);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // num0
+            // 
+            this.num0.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num0.Location = new System.Drawing.Point(14, 395);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(162, 61);
+            this.num0.TabIndex = 18;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            this.num0.Click += new System.EventHandler(this.click);
+            // 
+            // dot
+            // 
+            this.dot.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dot.Location = new System.Drawing.Point(176, 395);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(81, 61);
+            this.dot.TabIndex = 19;
+            this.dot.Text = ".";
+            this.dot.UseVisualStyleBackColor = true;
+            this.dot.Click += new System.EventHandler(this.click);
+            // 
+            // num3
+            // 
+            this.num3.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num3.Location = new System.Drawing.Point(176, 334);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(81, 61);
+            this.num3.TabIndex = 22;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            this.num3.Click += new System.EventHandler(this.click);
+            // 
+            // num1
+            // 
+            this.num1.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num1.Location = new System.Drawing.Point(14, 334);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(81, 61);
+            this.num1.TabIndex = 21;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            this.num1.Click += new System.EventHandler(this.click);
+            // 
+            // num2
+            // 
+            this.num2.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num2.Location = new System.Drawing.Point(95, 334);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(81, 61);
+            this.num2.TabIndex = 20;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            this.num2.Click += new System.EventHandler(this.click);
+            // 
+            // num6
+            // 
+            this.num6.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num6.Location = new System.Drawing.Point(176, 273);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(81, 61);
+            this.num6.TabIndex = 25;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            this.num6.Click += new System.EventHandler(this.click);
+            // 
+            // num4
+            // 
+            this.num4.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num4.Location = new System.Drawing.Point(14, 273);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(81, 61);
+            this.num4.TabIndex = 24;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            this.num4.Click += new System.EventHandler(this.click);
+            // 
+            // num5
+            // 
+            this.num5.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num5.Location = new System.Drawing.Point(95, 273);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(81, 61);
+            this.num5.TabIndex = 23;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            this.num5.Click += new System.EventHandler(this.click);
+            // 
+            // num9
+            // 
+            this.num9.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num9.Location = new System.Drawing.Point(176, 212);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(81, 61);
+            this.num9.TabIndex = 28;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            this.num9.Click += new System.EventHandler(this.click);
+            // 
+            // num7
+            // 
+            this.num7.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num7.Location = new System.Drawing.Point(14, 212);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(81, 61);
+            this.num7.TabIndex = 27;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            this.num7.Click += new System.EventHandler(this.click);
+            // 
+            // num8
+            // 
+            this.num8.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num8.Location = new System.Drawing.Point(95, 212);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(81, 61);
+            this.num8.TabIndex = 26;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            this.num8.Click += new System.EventHandler(this.click);
+            // 
+            // plus
+            // 
+            this.plus.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.plus.Location = new System.Drawing.Point(258, 212);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(56, 122);
+            this.plus.TabIndex = 30;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.click);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.minus.Location = new System.Drawing.Point(258, 334);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(56, 61);
+            this.minus.TabIndex = 31;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.click);
+            // 
+            // equal
+            // 
+            this.equal.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.equal.Location = new System.Drawing.Point(258, 396);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(113, 60);
+            this.equal.TabIndex = 32;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.Equal_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.clear.Location = new System.Drawing.Point(14, 151);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(81, 61);
+            this.clear.TabIndex = 33;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.divide.Location = new System.Drawing.Point(315, 273);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(56, 61);
+            this.divide.TabIndex = 34;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.click);
+            // 
+            // mult
+            // 
+            this.mult.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.mult.Location = new System.Drawing.Point(315, 212);
+            this.mult.Name = "mult";
+            this.mult.Size = new System.Drawing.Size(56, 61);
+            this.mult.TabIndex = 35;
+            this.mult.Text = "*";
+            this.mult.UseVisualStyleBackColor = true;
+            this.mult.Click += new System.EventHandler(this.click);
+            // 
+            // negative
+            // 
+            this.negative.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.negative.Location = new System.Drawing.Point(315, 334);
+            this.negative.Name = "negative";
+            this.negative.Size = new System.Drawing.Size(56, 61);
+            this.negative.TabIndex = 39;
+            this.negative.Text = "±";
+            this.negative.UseVisualStyleBackColor = true;
+            // 
+            // leftbrackets
+            // 
+            this.leftbrackets.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.leftbrackets.Location = new System.Drawing.Point(95, 151);
+            this.leftbrackets.Name = "leftbrackets";
+            this.leftbrackets.Size = new System.Drawing.Size(81, 61);
+            this.leftbrackets.TabIndex = 40;
+            this.leftbrackets.Text = "(";
+            this.leftbrackets.UseVisualStyleBackColor = true;
+            this.leftbrackets.Click += new System.EventHandler(this.click);
+            // 
+            // rightbrackets
+            // 
+            this.rightbrackets.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rightbrackets.Location = new System.Drawing.Point(176, 151);
+            this.rightbrackets.Name = "rightbrackets";
+            this.rightbrackets.Size = new System.Drawing.Size(81, 61);
+            this.rightbrackets.TabIndex = 41;
+            this.rightbrackets.Text = ")";
+            this.rightbrackets.UseVisualStyleBackColor = true;
+            this.rightbrackets.Click += new System.EventHandler(this.click);
+            // 
+            // back
+            // 
+            this.back.Font = new System.Drawing.Font("微軟正黑體", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.back.Location = new System.Drawing.Point(258, 151);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(113, 61);
+            this.back.TabIndex = 42;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.Location = new System.Drawing.Point(12, 17);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(357, 64);
+            this.textBox2.TabIndex = 43;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // MachiCaculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 461);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.rightbrackets);
+            this.Controls.Add(this.leftbrackets);
+            this.Controls.Add(this.negative);
+            this.Controls.Add(this.mult);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MachiCaculator";
+            this.Text = "Machi Caculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button dot;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button mult;
+        private System.Windows.Forms.Button negative;
+        private System.Windows.Forms.Button leftbrackets;
+        private System.Windows.Forms.Button rightbrackets;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ calculator01/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator01
+{
+    public partial class MachiCaculator : Form
+    {
+        public char[] signs = { '+', '-', '*', '/' };
+        public double[] num;//有哪些數字
+        public char[] sign;//有哪些運算符號
+        public int[] sign_position;//運算符號在哪些位置，分割數字用
+        public MachiCaculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+        private void click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            string formula = textBox1.Text;//輸入運算
+            char[] char_formula = formula.ToCharArray();//轉為字元陣列
+            sign = new char[Num_and_Sign(char_formula)];//運算符號陣列長度
+            sign_position = new int[sign.Length + 2];//運算符號位置的陣列長度
+            num = new double[sign.Length + 1];//數字的陣列長度
+            Sign_Position(char_formula);
+            textBox2.Text = formula;
+            textBox1.Text = Convert.ToString(Answer());
+        }
+        //判斷有幾個運算符號來指定陣列長度
+        public int Num_and_Sign(char[] formula)
+        {
+            int count = 0;
+            for (int i = 0; i < formula.Length; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (formula[i] == signs[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        //第幾位是運算符號，分別是什麼運算符號
+        //分割數字
+        public void Sign_Position(char[] formula)
+        {
+            string number;
+            for (int i = 0, k = 0; i < formula.Length; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    //第i位是運算符號時執行
+                    if (formula[i] == signs[j])
+                    {
+                        sign[k] = signs[j];//將運算符號存在陣列sign
+                        sign_position[k + 1] = i;//判斷第幾位是運算符號，以便分割數字
+                        k++;
+                    }
+                }
+            }
+
+            //設定sign_position頭尾的數字
+            sign_position[0] = -1;
+            sign_position[sign_position.Length - 1] = formula.Length;
+
+            //分割數字後存進num陣列
+            for (int i = 0; i < sign_position.Length - 1; i++)
+            {
+                number = new string(formula, sign_position[i] + 1, sign_position[i + 1] - sign_position[i] - 1);//分割數字
+                num[i] = Convert.ToDouble(number);//存進陣列
+            }
+        }
+
+        //先乘除後加減，回傳答案
+        public double Answer()
+        {
+            int count = 0;//已經連續做了幾個乘除運算
+            //先乘除
+            for (int i = 0; i < sign.Length; i++)
+            {
+                if (sign[i] == '*')
+                {
+                    num[i - count] = num[i - count] * num[i + 1];
+                    count++;
+                }
+                else if (sign[i] == '/')
+                {
+                    num[i - count] = num[i - count] / num[i + 1];
+                    count++;
+                }
+                else//若沒有連續乘除，count歸零
+                {
+                    count = 0;
+                }
+            }
+            //後加減
+            double ans = num[0];
+            for (int i = 0; i < sign.Length; i++)
+            {
+                if (sign[i] == '+')
+                {
+                    ans += num[i + 1];
+                }
+                else if (sign[i] == '-')
+                {
+                    ans -= num[i + 1];
+                }
+            }
+            return ans;
+        }
+    }
+}
